826d4677001b263a42ecea76fbf08105a11ad7a5
aributron <ari.butron95@gmail.com>
2022-10-02 20:40:53 -0300

    vistas basicas de login y register

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import NavigationBar from './components/NavigationBar'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+import Register from "./components/Register"
+import Home from "./components/Home"
 
 function App() {
   return (
-    <div>
-      <NavigationBar />
-    </div>
+    <Router>
+      <div>
+        <NavigationBar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route exact path="/Register" element={<Register/>}/>        
+      </Routes>
+    </Router>
   )
 }
 
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -4,6 +4,7 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Dropdown from 'react-bootstrap/Dropdown';
+import {BasicExample} from '../components/Register'
 
 function NavigationBar() {
     return (
@@ -26,7 +27,7 @@ function NavigationBar() {
                         style={{ maxHeight: '100px' }}
                         navbarScroll
                     >
-                        <Nav.Link href="#action1" >Home</Nav.Link>
+                        <Nav.Link href="/"> Home </Nav.Link>
                         <Nav.Link href="#action2" >Trending</Nav.Link>
                     </Nav>
                     <Nav>
@@ -34,10 +35,12 @@ function NavigationBar() {
                             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
                                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
                             </svg>
+                            <Nav.Link href="../Register"> Register </Nav.Link>
                             <Dropdown drop='down' align={{ lg: 'end' }} >
                                 <Dropdown.Toggle id="user-menu" variant="black text-white">
                                     <img className='rounded-circle me-2' src='https://i1.sndcdn.com/avatars-000138404358-sdp2xr-t500x500.jpg' style={{ maxHeight: '50px' }} ></img>
                                 </Dropdown.Toggle>
+
 
                                 <Dropdown.Menu variant="dark">
                                     <Dropdown.Item eventKey="2">Profile</Dropdown.Item>
