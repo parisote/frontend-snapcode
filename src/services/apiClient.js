34c453bbcb5ba46b6e6bb59a14884dfd1edd32b4
Nicolas Calezich <nicolas.calezich@globant.com>
2022-10-05 23:48:45 -0300

    Feat: se agrega header default para requests

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,6 +1,10 @@
 import axios from "axios";
+import AuthContext from '../context/Auth-context';
+
+const ctx = useContext(AuthContext)
 
 // TODO: Implementar .env 
-const apiClient = axios.create({baseURL: 'http://localhost:8080/'})
+const apiClient = axios.create({baseURL: 'http://localhost:8080/',
+headers: { 'authorization': 'Bearer ' + ctx.token ?? '' }})
 
 export default apiClient
