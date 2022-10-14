d4651c629d36ccc29c2abad909171e1fbea9d86c
Emiliano Goyeneche <emilianogoyeneche@gmail.com>
2022-10-13 22:27:02 -0300

    test

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 // TODO: Implementar .env 
-const apiClient = axios.create({baseURL: 'http://localhost:8080/'})
+const apiClient = axios.create({baseURL: 'http://localhost:5555/'})
 
 export default apiClient
