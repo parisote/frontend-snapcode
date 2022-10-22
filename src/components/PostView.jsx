df9be1b128632cb5c3be68d30fb85db514d75029
harushishi <juanmaac321123@gmail.com>
2022-10-22 19:25:22 -0300

    improvements del profile. tambien funcionalidad de likes a los posts

--- src/components/PostView.jsx
+++ src/components/PostView.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 // import AuthContext from '../context/Auth-context';
 import Editor from "@monaco-editor/react";
 import themeData from '../utils/monacoConfigs'
 import Tag from './Tag';
+import AuthContext from '../context/Auth-context';
+import apiClient from '../services/apiClient';
 
 function PostView(props) {
+  const ctx = useContext(AuthContext)
+  const { post } = props
+  const [profile, setProfile] = useState(null)
+  const [likes, setLikes] = useState(post.likedBy.length)
+  const [liked, setLiked] = useState(() => {
+    const userLiked = post.likedBy.find(e => e.id === ctx.userId)
+    if (userLiked) return true
+    return false
+  })
 
-  const { user, post } = props
   const date = new Date(post.createdAt)
+
+  const likePost = async () => {
+    const response = await apiClient.post(`/api/user/${ctx.userId}/like/post/${post.id}`)
+    if (response.status === 200) {
+      if (!liked) {
+        setLiked(() => !liked)
+        setLikes(() => (likes + 1))
+      } else {
+        setLiked(() => !liked)
+        setLikes(() => (likes - 1))
+      }
+    }
+  }
+
+  useEffect(() => {
+    apiClient.get(`/api/user/profile/${post.authorId}`).then(parseProfile)
+  }, []);
+
+  const parseProfile = (res) => setProfile(res.data)
 
   function handleEditorChange(value, event) {
     // here is the current value
@@ -36,13 +65,17 @@ function PostView(props) {
     )
   }
 
+  if (!profile) {
+    return <></>
+  }
+
   return (
     <div className='container-fluid text-white bg-black mt-3'>
       <div className='row gx-0'>
         <div className='d-flex'>
-          <img src={`https://vsa-bucket-test.s3.sa-east-1.amazonaws.com/${user.pfp}`} alt='alt' className='img-fluid rounded-circle' style={{ maxHeight: '40px' }}></img>
+          <img src={`https://vsa-bucket-test.s3.sa-east-1.amazonaws.com/${profile.pfp}`} alt='alt' className='img-fluid rounded-circle' style={{ maxHeight: '40px' }}></img>
           <p className='mx-1 mt-2 text-info'>
-            {user.username}
+            {profile.username}
           </p>
           <p className='mt-2 text-secondary'>/</p>
           <p className='mx-1 mt-2 fw-bold text-info'>
@@ -51,11 +84,18 @@ function PostView(props) {
           <p className='mx-1 fw-light fst-italic' style={{ fontSize: '0.8em', marginTop: '12px' }}>
             {date.toLocaleDateString() + ' - ' + date.toLocaleTimeString()}
           </p>
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-star mt-2 ms-auto text-info" viewBox="0 0 16 16">
-            <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
-          </svg>
+          {liked ?
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-star-fill mt-2 ms-auto text-info" viewBox="0 0 16 16" onClick={likePost}>
+              <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+            </svg> :
+            !liked &&
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-star mt-2 ms-auto text-info" viewBox="0 0 16 16" onClick={likePost}>
+              <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+            </svg>
+          }
+
           <p className='mt-2 mx-1 text-secondary'>
-            25
+            {likes}
           </p>
         </div>
         <div className='row gx-0'>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -14,7 +14,10 @@ const Profile = () => {
 
     const [user, setUser] = useState(null)
     const [profile, setProfile] = useState(null)
+    const [toggle, setToggle] = useState(false)
     const [posts, setPosts] = useState(null)
+    const [likedPosts, setLikedPosts] = useState(null)
+
     let userId
 
     if (!location.state) {
@@ -28,14 +31,16 @@ const Profile = () => {
         apiClient.get(`/api/user/${userId}`).then(parseUser)
         apiClient.get(`/api/user/profile/${userId}`).then(parseProfile)
         apiClient.get(`/api/post/user/${userId}`).then(parsePosts)
+        apiClient.get(`/api/post/user/liked/${userId}`).then(parseLikedPosts)
         // apiClient.get(`/api/user/following/${ctx.userId}`)
     }, []);
     const parseUser = (res) => setUser(res.data)
     const parseProfile = (res) => setProfile(res.data)
     const parsePosts = (res) => setPosts(res.data.post)
+    const parseLikedPosts = (res) => setLikedPosts(res.data[0].likedPosts)
 
-    if (!profile || !user || !posts) {
-        return <div className='bg-dark min-vh-100'>loading</div>
+    if (!profile || !user || !posts || !likedPosts) {
+        return <div className='bg-dark min-vh-100 text-white'>loading</div>
     }
 
     const data = {
@@ -46,7 +51,15 @@ const Profile = () => {
     const renderPosts = () => {
         const sortedPosts = sortPosts(posts, 'date')
         return (
-            <>{sortedPosts.map(post => (<PostView user={data} post={post} key={post.id} />))}</>
+            <>{sortedPosts.map(post => (<PostView post={post} key={post.id} />))}</>
+        )
+    }
+
+    const renderLikedPosts = () => {
+        const sortedPosts = sortPosts(likedPosts, 'date')
+        console.log(sortedPosts)
+        return (
+            <>{sortedPosts.map(post => (<PostView post={post} key={post.id} />))}</>
         )
     }
 
@@ -59,10 +72,14 @@ const Profile = () => {
                     <ProfileBar {...data} />
                 </div>
                 <div className="h-card col-md-7 mt-3">
-                    <ProfileTopBar />
-                    {renderPosts()}
-                    {/* {!posts && <p>loading...</p>}
-                    {posts && <p>hay posts</p>} */}
+                    <ProfileTopBar toggle={toggle} setToggle={setToggle} />
+                    {
+                        !toggle ?
+                            renderPosts()
+                            :
+                            toggle &&
+                            renderLikedPosts()
+                    }
                 </div>
             </div>
         </div>
