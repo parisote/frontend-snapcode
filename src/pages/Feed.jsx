f8ff414bca22d0b6ec8dc4be239357211cb38eb6
Nicolas Calezich <nicolas.calezich@globant.com>
2022-11-23 01:09:23 -0300

    se agrega filtro por fecha y limpiar filtros

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -15,9 +15,18 @@ const Feed = () => {
     const [posts, setPosts] = useState(null)
     const [showFilters, setShowFilters] = useState(false)
     const [titleFilter, setTitleFilter] = useState()
+    const [fromFilter, setFromFilter] = useState()
+    const [toFilter, setToFilter] = useState()
+    const [isFiltered, setIsFiltered] = useState(false)
+
 
     const handleCloseFilters = () => setShowFilters(false);
-    const handleShowFilters = () => setShowFilters(true);
+    const handleShowFilters = () => {
+        setTitleFilter()
+        setFromFilter()
+        setToFilter()
+        setShowFilters(true)
+    }
 
     let userId
 
@@ -42,12 +51,17 @@ const Feed = () => {
     }
 
     const applyFilters = () => {
-        console.log(titleFilter)
+        (!titleFilter && !fromFilter && !toFilter) ? setIsFiltered(false) : setIsFiltered(true)
         handleCloseFilters()
-        apiClient.get(`/api/post/user/feed/filter/${userId}?title=${titleFilter}&from=null&to=null`).then(parsePosts)
+        apiClient.get(`/api/post/user/feed/filter/${userId}?title=${titleFilter}&from=${fromFilter}&to=${toFilter}`).then(parsePosts)
         renderPosts()
     }
 
+    const clearFilters = () => {
+        setIsFiltered(false)
+        apiClient.get(`/api/post/user/feed/${userId}`).then(parsePosts)
+        renderPosts()
+    }
     //esto y lo proximo deberia ir idealmente en un hook
     useEffect(() => {
         apiClient.get(`/api/user/${userId}`).then(parseUser)
@@ -77,23 +91,30 @@ const Feed = () => {
                                 className="me-2 bg-black border-0 rounded text-light w-40 p-2"
                                 aria-label="Search"
                                 size='sm'
-                                onChange={(e) => setTitleFilter(e.target.value)}
+                                onChange={(e) => setTitleFilter(e.target.value.trim())}
                             />
                             <label className='text-light'>from</label>
-                            <input type="date" className='m-1 p-2 bg-black border-0 rounded text-light'/>
+                            <input onChange={(e) => setFromFilter(e.target.value)} type="date" className='m-1 p-2 bg-black border-0 rounded text-light'/>
                             <label className='text-light'>to</label>
-                            <input type="date" className='m-1 p-2 bg-black border-0 rounded text-light'/>
+                            <input onChange={(e) => setToFilter(e.target.value)} type="date" className='m-1 p-2 bg-black border-0 rounded text-light'/>
                             <button onClick={applyFilters} className="btn btn-dark m-2 border-secondary">Apply</button>
                         </div>
                     </Modal.Body>
             </Modal>
             <div className='bg-black text-light'>
                 <button onClick={handleShowFilters} className="btn btn-dark m-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-filter-left" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-filter-left mx-1" viewBox="0 0 16 16">
                         <path d="M2 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
                     </svg>
                     filters
                 </button>
+                {isFiltered? <button onClick={clearFilters} className="btn btn-danger m-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-backspace mx-1" viewBox="0 0 16 16">
+                                    <path d="M5.83 5.146a.5.5 0 0 0 0 .708L7.975 8l-2.147 2.146a.5.5 0 0 0 .707.708l2.147-2.147 2.146 2.147a.5.5 0 0 0 .707-.708L9.39 8l2.146-2.146a.5.5 0 0 0-.707-.708L8.683 7.293 6.536 5.146a.5.5 0 0 0-.707 0z"/>
+                                    <path d="M13.683 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-7.08a2 2 0 0 1-1.519-.698L.241 8.65a1 1 0 0 1 0-1.302L5.084 1.7A2 2 0 0 1 6.603 1h7.08zm-7.08 1a1 1 0 0 0-.76.35L1 8l4.844 5.65a1 1 0 0 0 .759.35h7.08a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1h-7.08z"/>
+                                </svg>
+                                clear filters
+                                </button> : <></>}
                 <div className="d-flex align-items-center justify-content-center min-vh-100">
                     <div className='d-flex align-items-start w-75 min-vh-100'>
                         <div className="col-md-2 mt-2">
